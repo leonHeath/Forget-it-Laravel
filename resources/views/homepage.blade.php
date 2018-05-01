eb540f09df9c7203226dea6e6ec297dddbafc145
Leon Heath <leon.heath13@gmail.com>
2018-05-01 15:54:57 -0500

    Create Task migration and Task class. Added Facades to phpstorm.

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leon
+ * Date: 2018-05-01
+ * Time: 11:48 AM
+ */
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
