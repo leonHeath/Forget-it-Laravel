3e571c7d41c23de82e152c45474c36c04583c31a
Leon Heath <leon.heath13@gmail.com>
2018-05-24 19:20:11 -0500

    Include ID with task

--- resources/views/tasks/task.blade.php
+++ resources/views/tasks/task.blade.php
@@ -2,6 +2,7 @@
     <div class="flex-center-parent task-header">
         <div>
             <h4>{{ $task->title }}</h4>
+            <input type="hidden" id="task_id" name="task_id" value={{ $task->id }}>
         </div>
         <button class="edit-task" data-toggle="modal" data-target="#edit-task-modal"></button>
     </div>
